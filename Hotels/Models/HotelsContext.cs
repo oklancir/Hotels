8abe3f4918c147ed28483486a17a167e4c8c6ffb
Ozren Klančir <ozren.klancir@omega-software.hr>
2018-12-11 15:46:59 +0100

    Added DB migrations

--- Hotels/Models/HotelsContext.cs
+++ Hotels/Models/HotelsContext.cs
@@ -10,7 +10,15 @@ namespace Hotels.Models
     {
         public HotelsContext() : base("name=HotelsDbContext")
         {
-            
         }
+
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<ReservationStatus> ReservationStatuses { get; set; }
+        public DbSet<ServiceProduct> ServiceProducts { get; set; }
     }
 }
--- Hotels/Models/Item.cs
+++ Hotels/Models/Item.cs
@@ -7,6 +7,7 @@ namespace Hotels.Models
 {
     public class Item
     {
+        public int Id { get; set; }
         public int Amount { get; set; }
         public virtual Invoice Invoice { get; set; }
         public virtual ServiceProduct ServiceProduct { get; set; }
