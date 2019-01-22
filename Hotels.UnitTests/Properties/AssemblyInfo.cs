bb5c8093abfe5a9c638084879708eddac9e9b96a
Ozren Klančir <ozren.klancir@omega-software.hr>
2019-01-22 17:19:38 +0100

    added Unit Tests project to the solution

---
+++ Hotels.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hotels.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Omega software d.o.o.")]
+[assembly: AssemblyProduct("Hotels.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Omega software d.o.o. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73a8b68c-c486-41e6-b77e-2323ad95c8c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
