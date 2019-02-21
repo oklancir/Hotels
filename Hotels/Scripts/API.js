66ca603b200a8866e2ce8aed77e40abccbc218d2
Ozren Klančir <ozrenklancir@gmail.com>
2019-02-21 22:30:03 +0100

    added API.js file

---
+++ Hotels/Scripts/API.js
@@ -0,0 +1,67 @@
+var API = {
+
+    Rooms: {
+        get: function (id, success, error) {
+
+        },
+        update: function (id, success, error) {
+
+        },
+        delete: function (id, success, error) {
+
+        },
+        create: function (data, success, error) {
+
+        }
+    },
+    
+    Guests: {
+        get: function (id, success, error) {
+
+        },
+        update: function (id, success, error) {
+
+        },
+        delete: function (id, success, error) {
+
+        },
+        create: function (data, success, error) {
+
+        }
+    },
+
+    Reservations: {
+        get: function (id, success, error) {
+
+        },
+        update: function (id, success, error) {
+
+        },
+        delete: function (id, success, error) {
+
+        },
+        create: function (data, success, error) {
+
+        }
+    },
+
+    Items: {
+        get: function (id, success, error) {
+
+        },
+        update: function (id, success, error) {
+
+        },
+        delete: function (id, success, error) {
+
+        },
+        create: function (data, success, error) {
+
+        }
+    }
+};
+
+
+var room = new Room("mouse")
+
+API.Reservations.create(reservation);
