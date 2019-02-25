4b8ff476a5bef83432b1aeef777e78fbd4841183
Ozren Klancir <oklancir@banelli.biz>
2019-02-25 19:30:46 +0100

    almost done!

--- Hotels/Scripts/API.js
+++ Hotels/Scripts/API.js
@@ -1,67 +1,186 @@
-var API = {
-
+﻿var API = {
     Rooms: {
         get: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/rooms/" + id,
+                method: "GET",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
-        update: function (id, success, error) {
-
+        update: function (room, success, error) {
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "PUT",
+                data: room,
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         delete: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "DELETE",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
-        create: function (data, success, error) {
-
+        create: function (room, success, error) {
+            $.ajax({
+                url: "/api/reservations",
+                method: "POST",
+                data: room,
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         }
     },
-    
     Guests: {
         get: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/guests/" + id,
+                method: "GET",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         update: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "PUT",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         delete: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "DELETE",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         create: function (data, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "POST",
+                data: data,
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         }
     },
-
     Reservations: {
         get: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "GET",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         update: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "PUT",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         delete: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "DELETE",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         create: function (data, success, error) {
-
+            $.ajax({
+                url: "/api/reservations/" + id,
+                method: "POST",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         }
     },
-
     Items: {
         get: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/items/" + id,
+                method: "GET",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         update: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/items/" + id,
+                method: "PUT",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         delete: function (id, success, error) {
-
+            $.ajax({
+                url: "/api/items/" + id,
+                method: "DELETE",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         },
         create: function (data, success, error) {
-
+            $.ajax({
+                url: "/api/items/" + id,
+                method: "POST",
+                data: data,
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
+        }
+    },
+    Invoices: {
+        get: function (id, success, error) {
+            $.ajax({
+                url: "/api/invoices/" + id,
+                method: "GET",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
+        },
+        update: function (id, success, error) {
+            $.ajax({
+                url: "/api/items/" + id,
+                method: "PUT",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
+        },
+        delete: function (id, success, error) {
+            $.ajax({
+                url: "/api/items/" + id,
+                method: "DELETE",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
+        },
+        create: function (data, success, error) {
+            $.ajax({
+                url: "/api/items/" + id,
+                method: "POST",
+                success: function (data) { success(data); },
+                error: function (xhr, options, errorThrown) { error(errorThrown); }
+            });
         }
     }
+}
+
+var testSuccess = function (data) {
+    console.log("Reservation " + data.id + " successfully returned");
 };
 
+var testError = function (data) {
+    console.log("Reservation " + data.id + "does not exist!");
+};
 
-var room = new Room("mouse")
-
-API.Reservations.create(reservation);
+API.Rooms.get(1, testSuccess, testError);
--- Hotels/Scripts/Reservations.js
+++ Hotels/Scripts/Reservations.js
@@ -29,7 +29,7 @@
             {
                 data: "id",
                 render: function (data) {
-                    return "<button class='btn-link js-edit' data-reservation-id=" + data + ">Edit</button>";
+                    return "<button class='btn-link js-edit' data-toggle='modal' data-target='#edit-reservation-modal' data-reservation-id=" + data + ">Edit</button>";
                 }
             },
             {
@@ -43,37 +43,44 @@
 
     $("#reservations").on("click", ".js-delete", function () {
         var button = $(this);
-        var buttonAttr = button.attr("data-reservation-id");
+        var reservationId = button.attr("data-reservation-id");
 
         bootbox.confirm("Are you sure you want to delete this reservation?", function (result) {
             if (result) {
-                $.ajax({
-                    url: "/api/reservations/" + buttonAttr,
-                    method: "DELETE",
-                    success: function () {
-                        button.parents("tr").remove();
+                API.Reservations.delete(
+                    reservationId,
+                    function() {
+                        button.parents('tr').remove();
+                    },
+                    function(xhr, options, error) {
+                        alert(error);
                     }
-                });
+                );
             }
         });
     });
 
-    $("#reservations").on("click", ".js-edit", function () {
+    $('#edit-reservation-modal #saveChanges').on('click', function (event) {
         var button = $(this);
-
-        bootbox.confirm("Are you sure you want to edit this reservation?", function (result) {
-            if (result) {
-                API.Reservations.get(button.attr)
-                $.ajax({
-                    url: "/api/reservations/" + button.attr("data-reservation-id"),
-                    method: "GET",
-                    success: function () {
-                    }
-                });
-            }
-        });
+        var modal = button.parents('#edit-reservation-modal');
+        var reservationId = parseInt(modal.find('input#reservationId')[0].value);
+        console.log(reservationId);
+        // API.Reservations.update()
     });
 
+    $('#edit-reservation-modal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget);
+        var row = button.parents('tr');
+        var table = row.parents('table');
+        var reservation = table.DataTable().rows(row).data()[0];
+
+        var modal = $(this);
+        modal.find('.modal-body input#reservationId').val(reservation.id);
+        modal.find('.modal-body input#startDate').val(reservation.startDate);
+        modal.find('.modal-body input#endDate').val(reservation.endDate);
+
+    });
+    
     $("#reservations").on("click", ".js-details", function () {
         var button = $(this);
 
